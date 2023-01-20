f05cfdbfe9305026dab36879d5f463d762c38fac
Teddy van Jerry <me@teddy-van-jerry.org>
2023-01-20 16:43:39 +0800

    Initial Maintainence Tool

---
+++ tools/maintain/maintain.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file maintain.cpp
+ * @author Wuqiong Zhao (wqzhao@seu.edu.cn)
+ * @brief mmCEsim Maintainence Tool
+ * @version 0.2.0
+ * @date 2023-01-20
+ * 
+ * @copyright Copyright (c) 2023 Wuqiong Zhao (Teddy van Jerry)
+ * 
+ */
+
+#include "meta.h"
+#include <iostream>
+#include <boost/exception/diagnostic_information.hpp>
+#include <boost/program_options.hpp>
+
+#ifdef _MSC_VER
+#    define _CRT_SECURE_NO_WARNINGS
+#    pragma warning(disable : 4244)
+#endif
+
+int main(int argc, char* argv[]) {
+    namespace po = boost::program_options;
+
+    po::options_description generic("Generic options");
+    generic.add_options()
+        ("version,v", "print version string")
+        ("help,h", "produce help message")
+    ;
+
+    std::cout << _MMCESIM_VER_STR << std::endl;
+    return 0;
+}
