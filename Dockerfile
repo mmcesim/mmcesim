fae8ef2fe0a75d164c809798dab479488c6b1198
Teddy van Jerry <me@teddy-van-jerry.org>
2023-01-20 17:08:50 +0800

    Update Dockerfile
    Copy Files for installation.

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,13 @@ FROM ubuntu:20.04
 ARG DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && apt-get install -y build-essential cmake libboost-all-dev
 WORKDIR /mmcesim/build
+RUN mkdir ../doc
 COPY CMakeLists.txt ..
+COPY LICENSE ..
+COPY README.md ..
+COPY CONTRIBUTING.md ..
+COPY CODE_OF_CONDUCT.md ..
+COPY doc/mmCEsim-doc.pdf ../doc
 COPY src ../src
 COPY include ../include
 COPY test ../test
--- tools/maintain/maintain.cpp
+++ tools/maintain/maintain.cpp
@@ -4,15 +4,15 @@
  * @brief mmCEsim Maintainence Tool
  * @version 0.2.0
  * @date 2023-01-20
- * 
+ *
  * @copyright Copyright (c) 2023 Wuqiong Zhao (Teddy van Jerry)
- * 
+ *
  */
 
 #include "meta.h"
-#include <iostream>
 #include <boost/exception/diagnostic_information.hpp>
 #include <boost/program_options.hpp>
+#include <iostream>
 
 #ifdef _MSC_VER
 #    define _CRT_SECURE_NO_WARNINGS
@@ -23,10 +23,7 @@ int main(int argc, char* argv[]) {
     namespace po = boost::program_options;
 
     po::options_description generic("Generic options");
-    generic.add_options()
-        ("version,v", "print version string")
-        ("help,h", "produce help message")
-    ;
+    generic.add_options()("version,v", "print version string")("help,h", "produce help message");
 
     std::cout << _MMCESIM_VER_STR << std::endl;
     return 0;
