119b3c4debac7e3644f2a29b517d7beb1730fd6f
Teddy van Jerry <me@teddy-van-jerry.org>
2023-03-17 00:42:57 +0800

    Configure `mmcesim-log` CMake Structure (#37)

--- tools/log/log.cpp
+++ tools/log/log.cpp
@@ -1,5 +1,17 @@
+/**
+ * @file log.cpp
+ * @author Wuqiong Zhao (wqzhao@seu.edu.cn)
+ * @brief mmCEsim Log Tool
+ * @version 0.2.0
+ * @date 2023-03-17
+ *
+ * @copyright Copyright (c) 2023 Wuqiong Zhao (Teddy van Jerry)
+ *
+ */
+
 #include "log.h"
 
 int main() {
+    std::cout << "This is mmCEsim Log Tool!" << std::endl;
     return 0;
 }
---
+++ tools/log/log.h
@@ -0,0 +1,13 @@
+/**
+ * @file log.h
+ * @author Wuqiong Zhao (wqzhao@seu.edu.cn)
+ * @brief mmCEsim Log Tool
+ * @version 0.2.0
+ * @date 2023-03-17
+ *
+ * @copyright Copyright (c) 2023 Wuqiong Zhao (Teddy van Jerry)
+ *
+ */
+
+#include <clip.h>
+#include <iostream>
