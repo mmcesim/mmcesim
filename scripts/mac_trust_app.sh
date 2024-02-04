b34e0f44df7b4a1b9a0eb51fa2c241107f14de93
Teddy van Jerry <me@teddy-van-jerry.org>
2024-02-04 23:53:33 +0800

    Update Mac Trust App Shell Script (#103)
    Part of #95.

--- scripts/mac_trust_app.sh
+++ scripts/mac_trust_app.sh
@@ -6,7 +6,11 @@
 # allow as exectuable
 chmod a+x ./bin/astyle
 chmod a+x ./bin/mmcesim
+chmod a+x ./bin/mmcesim-log
+chmod a+x ./bin/mmcesim-maintain
 
 # disable macOS developer sign check
 xattr -r -d com.apple.quarantine ./bin/astyle
 xattr -r -d com.apple.quarantine ./bin/mmcesim
+xattr -r -d com.apple.quarantine ./bin/mmcesim-log
+xattr -r -d com.apple.quarantine ./bin/mmcesim-maintain
